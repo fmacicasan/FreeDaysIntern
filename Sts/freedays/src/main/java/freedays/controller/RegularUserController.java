8f9de367b3a2b5cd564062e0e58a8a392738f4a4
Iulia Teglas <iteglas@sdl.com>
2011-07-12 10:41:23 +0300

    added controller

---
+++ Sts/freedays/src/main/java/freedays/controller/RegularUserController.java
@@ -0,0 +1,12 @@
+package freedays.controller;
+
+import freedays.domain.RegularUser;
+import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RooWebScaffold(path = "regularusers", formBackingObject = RegularUser.class)
+@RequestMapping("/regularusers")
+@Controller
+public class RegularUserController {
+}
