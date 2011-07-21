714ab72f6aa856cedfc29dc5ddace6e6debe3a7b
gparvu <gparvu@clujgparvu01.global.sdl.corp>
2011-07-21 11:25:56 +0300

    nada

--- Sts/freeday/src/main/java/freedays/controller/AuthentificationController.java
+++ Sts/freeday/src/main/java/freedays/controller/AuthentificationController.java
@@ -1,47 +1,62 @@
 package freedays.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.NonUniqueResultException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.util.StringUtils;
 
 import freedays.domain.RegularUser;
 
-public class AuthentificationController extends AbstractUserDetailsAuthenticationProvider{
-	
+public class AuthentificationController extends
+		AbstractUserDetailsAuthenticationProvider  {
+	private RegularUser regularUser;
+
 	@Override
-	protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException{
-		//TODO Auto-generated method stub
+	protected void additionalAuthenticationChecks(UserDetails userDetails,
+			UsernamePasswordAuthenticationToken authentication)
+			throws AuthenticationException {
+		// TODO Auto-generated method stub
 	}
-	
+
 	@Override
-	protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException{
-		String password = (String)authentication.getCredentials();
-		if(!StringUtils.hasText(password)){
+	protected UserDetails retrieveUser(String username,
+			UsernamePasswordAuthenticationToken authentication)
+			throws AuthenticationException {
+		String password = (String) authentication.getCredentials();
+		if (!StringUtils.hasText(password)) {
 			throw new BadCredentialsException("Please enter the password!");
 		}
 		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-		try{
-			RegularUser regularuser = RegularUser.findRegularUsersByUsernameAndPasswordEquals(username, password).getSingleResult();
+		try {
+			regularUser = RegularUser
+					.findRegularUsersByUsernameAndPasswordEquals(username,
+							password).getSingleResult();
 			authorities.add(new GrantedAuthorityImpl("ROLE_USER"));
-		}catch (EmptyResultDataAccessException e){
+		} catch (EmptyResultDataAccessException e) {
 			throw new BadCredentialsException("Invalid username or password");
-		}catch (EntityNotFoundException e){
+		} catch (EntityNotFoundException e) {
 			throw new BadCredentialsException("Invalid user");
-		}catch (NonUniqueResultException e){
-			throw new BadCredentialsException("Non-unique user, contact administrator");
+		} catch (NonUniqueResultException e) {
+			throw new BadCredentialsException(
+					"Non-unique user, contact administrator");
 		}
 		return new User(username, password, true, true, true, true, authorities);
 	}
