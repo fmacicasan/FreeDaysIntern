1c152aeadbd35f96acfbd75d9de44664ffa50d13
gparvu <gparvu@clujgparvu01.global.sdl.corp>
2011-07-22 11:45:37 +0300

    added to track the update controller

---
+++ Sts/freeday/src/main/java/freedays/controller/UpdateRegularUserController.java
@@ -0,0 +1,52 @@
+package freedays.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.UriUtils;
+import org.springframework.web.util.WebUtils;
+
+import freedays.domain.RegularUser;
+
+@RequestMapping("/updateRegularUsers")
+@Controller
+public class UpdateRegularUserController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String createForm(Model uiModel,
+			HttpServletRequest httpServletRequest) {
+		
+		RegularUser regularUser = RegularUser.findRegularUsersByUsername(
+				httpServletRequest.getUserPrincipal().getName())
+				.getSingleResult();
+		
+		uiModel.addAttribute("regularUser", regularUser);
+		return "regularusers/update";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(HttpServletRequest httpServletRequest) {
+		String userID = (String) httpServletRequest.getSession().getAttribute(
+				"userID");
+		return "redirect:/regularusers/"
+				+ encodeUrlPathSegment(userID, httpServletRequest) + "?form";
+	}
+
+	private String encodeUrlPathSegment(String pathSegment,
+			HttpServletRequest httpServletRequest) {
+		String enc = httpServletRequest.getCharacterEncoding();
+		if (enc == null) {
+			enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
+		}
+		try {
+			pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
+		} catch (UnsupportedEncodingException uee) {
+		}
+		return pathSegment;
+	}
+}
