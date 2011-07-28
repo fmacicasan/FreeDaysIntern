16dc5f64b728bacdaf1caa10e14fa114bfbbd37b
fmacicasan <flo.macicasan@gmail.com>
2011-07-28 11:55:45 +0300

    Edited Sts/freeday/src/main/java/freedays/validation/RegularUserValidator.java via GitHub

--- Sts/freeday/src/main/java/freedays/validation/RegularUserValidator.java
+++ Sts/freeday/src/main/java/freedays/validation/RegularUserValidator.java
@@ -5,8 +5,7 @@ import freedays.domain.RegularUser;
 public class RegularUserValidator {
 
 	public static boolean validate(RegularUser regularUser) {
-		//TODO: add the code you forgot to put
-		return true;
+		return RegularUser.countRegularUserByEmail(regularUser.getEmail()) == 0;
 	}
 
 }
