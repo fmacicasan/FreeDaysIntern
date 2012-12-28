8531fe3a06324a97c71e04a1dc584aa59ee8cc41
fmacicasan <flo.macicasan@gmail.com>
2012-12-28 15:10:59 +0200

    removed osuciu from default mailing list.

--- Sts/freeday/src/main/java/freedays/util/MailUtils.java
+++ Sts/freeday/src/main/java/freedays/util/MailUtils.java
@@ -125,7 +125,6 @@ public class MailUtils {
 	}
 
 	private void finalizeTo(final List<String> tol) {
-		tol.add("osuciu@sdl.com");
 		tol.add("fmacicasan@sdl.com");
 		// add HR ppl to email flows
 		// tol.addAll(ApplicationRegularUser.findAllHRManagementEmails());
